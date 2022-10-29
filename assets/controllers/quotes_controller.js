36bde5f8446e3e62e4c77ca74154d4587f3cfeed
Zoran <zoran.coder@gmail.com>
2022-10-29 12:22:00 +0200

    FE first concrete lines

--- assets/controllers/quotes_controller.js
+++ assets/controllers/quotes_controller.js
@@ -10,7 +10,38 @@ import { Controller } from '@hotwired/stimulus';
  * Delete this file or adapt it for your use!
  */
 export default class extends Controller {
-    connect() {
-        this.element.textContent = 'Hello Stimulus!3';
+    static targets = [ "company_symbol", "start_date", "end_date", "email", "tmp_output" ]
+
+    // connect() {
+    //     this.element.textContent = 'Hello Stimulus!3';
+    // }
+
+    fetch() {
+
+        // todo prevent flooding
+
+        // BUSINESS LOGIC
+
+        // pass input
+
+        // validate
+            // validate FE
+               // if valid
+                   // request BE
+               // else
+                   // continue
+
+        // PRESENTATION LOGIC
+
+        // if not valid
+            // show errors
+        // else
+            // continue
+
+        // draw table
+        // draw chart
+
+        this.tmp_outputTarget.textContent =
+            `${this.company_symbolTarget.value} ${this.start_dateTarget.value} ${this.end_dateTarget.value} ${this.emailTarget.value}`
     }
 }
